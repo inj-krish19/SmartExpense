5ab1dbebf1573f14d90ef227a050f74fd9cc1934
inj-krish19 <kglivee19@gmail.com>
2025-11-04 21:33:40 +0530

    Defining the CLIENT ID in config

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,1 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL  :  "http://localhost:5000";                // Local backend
+export const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "http://localhost:5000";                // Local backend
+export const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID ? import.meta.env.VITE_GOOGLE_CLIENT_ID : "SOMECLIENTID";                // Local backend
